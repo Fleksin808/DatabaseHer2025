7a9a3467a032492a5c1c77f3f67d8b114ede9c82
Fleksin808 <fleky.st3@gmail.com>
2025-04-02 01:22:14 +0200

    Test02

--- Semestrální projekt/Program.cs
+++ Semestrální projekt/Program.cs
@@ -304,7 +304,7 @@ namespace Semestrální_projekt
                 else
                 {
                     Console.WriteLine("Zadal jsi neplatný vstup");
-                    Console.WriteLine("Velmi smutni");
+                    Console.WriteLine("A");
                 }      
             }
         }
